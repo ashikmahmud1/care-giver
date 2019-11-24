e305d2ceb6c0267f4c4eade7e7c8fe50977b74b3
Ashik Mahmud <ashikmahmud137@gmail.com>
2019-11-25 00:34:20 +0600

    Stepper component is properly rendered

--- src/views/Profile/Caregiver/index.js
+++ src/views/Profile/Caregiver/index.js
@@ -4,9 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
 import Steppers from "../../../components/Steppers/index";
-import CaregiverPersonalDetails from "../../../components/ProfileViews/CaregiverPersonalDetails/index";
-import CaregiverEduAndCerification from "../../../components/ProfileViews/CaregiverEduAndCertification/index";
-import CaregiverSkills from "../../../components/ProfileViews/CaregiverSkills/index";
+
 
 export default function ProfileCaregiver() {
   return (
@@ -15,9 +13,6 @@ export default function ProfileCaregiver() {
       <Container fixed>
         <Typography component="div" style={{ height: "10vh" }} />
         <Steppers />
-        <CaregiverPersonalDetails />
-        <CaregiverSkills />
-        <CaregiverEduAndCerification />
       </Container>
     </div>
   );
